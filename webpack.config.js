6298fdd03bf6f8fdfded6a71ce48d3002ca2769c
aiasfina <aiasfina@hotmail.com>
2017-03-12 01:01:33 +0800

    使用 webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+﻿module.exports = {
+  entry: './www/src/entry.js',
+  output: {
+    path: './www/scripts',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+　　　　　test: /\.(png|jpg)$/,
+　　　　　loader: 'url-loader?limit=8192'
+　　　　}
+    ]
+  }
+}
---
+++ www/src/entry.js
@@ -0,0 +1,11 @@
+﻿require('../css/app.scss')
+
+import m from 'mithril'
+
+document.addEventListener( 'deviceready', init, false)
+
+function init() {
+  m.route(document.body, '/topics', {
+    '/topics': {}
+  })
+}

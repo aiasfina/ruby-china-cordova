7e8028ea119085fde6d3bbd98cd35263bd6b0537
aiasfina <aiasfina@gmail.com>
2017-03-18 02:55:07 +0800

    删除滚动隐藏 tab

--- www/src/components/share/headerPanel.jsx
+++ www/src/components/share/headerPanel.jsx
@@ -1,46 +1,10 @@
 import '../../../css/header_panel.scss'
 
 import m from 'mithril'
-import move from 'move-js'
-
-function listenScroll(vnode) {
-  if (!vnode.attrs.fixed) { return }
-
-  var
-    dom = vnode.dom,
-    isShown = true,
-    lastScrollTop = 0
-
-  window.onscroll = (e) => {
-    var st = window.pageYOffset || document.documentElement.scrollTop
-    if (st - lastScrollTop > 60 && isShown) {
-      move(dom)
-        .duration(200)
-        .translate(0, -dom.offsetHeight)
-        .end()
-      isShown = false
-    } else if (lastScrollTop - st > 60 && !isShown) {
-      move(dom)
-        .duration(200)
-        .translate(0, 0)
-        .end()
-      isShown = true
-    }
-
-    if (timer) { clearTimeout(timer) }
-    var timer = setTimeout(() => {
-      lastScrollTop = st
-    }, 200)
-  }
-}
 
 const setContentHeight = vnode => {
   var height = vnode.dom.offsetHeight
   vnode.dom.parentNode.style.height = height + 'px'
-}
-
-const onremove = vnode => {
-  window.onscroll = null
 }
 
 const view = vnode => {
@@ -51,7 +15,7 @@ const view = vnode => {
     <div className="pe-header_panel--wrapper">
       <div className={'pe-header_panel' + (fixed ? ' fixed' : '')} oncreate={setContentHeight}>
         {attrs.toolbar}
-        <div oncreate={listenScroll} fixed={fixed}>
+        <div fixed={fixed}>
           {vnode.children}
         </div>
       </div>
@@ -60,6 +24,5 @@ const view = vnode => {
 }
 
 export default {
-  onremove,
   view
 }

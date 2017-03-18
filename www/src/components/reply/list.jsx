bbe3b034724936b5829c2316a1baed02ae82554c
aiasfina <aiasfina@gmail.com>
2017-03-18 16:15:00 +0800

    回复列表使用滚动加载

--- www/src/components/reply/list.jsx
+++ www/src/components/reply/list.jsx
@@ -2,14 +2,11 @@ import '../../../css/replies.scss'
 
 import m from 'mithril'
 import {list, tab, toolbar, iconButton, dialog} from 'polythene'
+import Scrollload from 'Scrollload'
 import iconArrowLeft from 'mmsvg/templarian/msvg/arrow-left'
 import ReplyItem from './item.jsx'
 import {loadList} from '../../controllers/replies'
 import HeaderPanel from '../share/headerPanel.jsx'
-
-const btnToBack = () => {
-  window.history.back()
-}
 
 const Toolbar = {
   iBtn: (msvg, cb) => {
@@ -25,7 +22,7 @@ const Toolbar = {
   },
   oninit: vnode => {
     vnode.state.btns = [
-      Toolbar.iBtn(iconArrowLeft, btnToBack),
+      Toolbar.iBtn(iconArrowLeft, () => { window.history.back() }),
       m('span.flex', vnode.attrs.topicTitle)
     ]
   },
@@ -40,30 +37,73 @@ const Toolbar = {
   }
 }
 
-const oninit = vnode => {
-  vnode.state.replies = []
+const vm = {
+  offset: 0,
+  list: [],
+  loadList: loadList
+}
 
-  loadList(vnode.attrs.topicId)
+const loadMore = vm => {
+  return vm.loadList(vm.topicId, vm.offset)
   .then(resp => {
-    vnode.state.replies = resp.replies
+    vm.list = vm.list.concat(resp.replies)
+    vm.offset += resp.replies.length
+
+    return resp.replies.length
   })
 }
 
+const createInfiniteList = (vnode) => {
+  const
+    loadMore = vnode.state.loadMore,
+    refresh = vnode.state.refresh,
+    vm = vnode.state.vm
+
+  return new Scrollload({
+    container: vnode.dom,
+    content: vnode.dom.querySelector('ul'),
+    threshold: 0,
+    loadMore: sl => {
+      loadMore(vm).then(length => {
+        if (!length) {
+          sl.noMoreData()
+        } else {
+          sl.unLock()
+        }
+      })
+    }
+  })
+}
+
+const oninit = vnode => {
+  const state = vnode.state
+
+  vm.topicId = vnode.attrs.topicId
+  state.vm = vm
+  state.loadMore = loadMore
+}
+
+const oncreate = vnode => {
+  vnode.state.infiniteList = createInfiniteList(vnode)
+}
+
 const view = vnode => {
-  const topicId = vnode.attrs.topicId
   const topicTitle = vnode.attrs.topicTitle
 
   return(
     <div className="app-topic_replies">
       {m(HeaderPanel, {toolbar: m(Toolbar, {topicTitle: topicTitle}), fixed: true})}
-      {m(list, {
-        tiles: vnode.state.replies.map(reply => { return m(ReplyItem, {reply: reply}) })
-      })}
+      <ul>
+        {vnode.state.vm.list.map(reply => {
+          return m(ReplyItem, {reply: reply})
+        })}
+      </ul>
     </div>
   )
 }
 
 export default {
   oninit,
+  oncreate,
   view
 }
--- www/src/controllers/replies.js
+++ www/src/controllers/replies.js
@@ -1,10 +1,11 @@
 import m from 'mithril'
 
-const loadList = (topic_id) => {
+const loadList = (topic_id, offset) => {
 
   return m.request({
     method: 'GET',
-    url: 'https://ruby-china.org/api/v3/topics/' + topic_id + '/replies.json'
+    url: 'https://ruby-china.org/api/v3/topics/' + topic_id + '/replies.json',
+    data: {offset: offset}
   })
 }
 
